56a43f55f90c8936157ba83cf93a642572cea678
Alumno_FP <jorgeotin@gmail.com>
2023-02-01 13:48:35 +0100

    V1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,20 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import com.aspose.cells.TxtLoadOptions;
+import com.aspose.cells.Workbook;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        // This code example demonstrates how to convert a CSV file to an XML with options.
+// Initialize TXT load options
+        TxtLoadOptions options = new TxtLoadOptions();
+        options.setMultiEncoded(true);
+
+// Load the CSV file into Workbook
+        Workbook workbook = new Workbook("src/main/resources/proyectos.csv", options);
+
+// Save it as XML
+        workbook.save("src/main/resources/proyectos.xml");
     }
 }
